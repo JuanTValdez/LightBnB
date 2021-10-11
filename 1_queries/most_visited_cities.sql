a737c76058eb12323440b18adeac0c5e30a43b22
Juan <juantvaldez85.@gmail.com>
2021-10-11 19:36:49 +0000

    added all 1_queries

---
+++ 1_queries/most_visited_cities.sql
@@ -0,0 +1,16 @@
+-- lighthouse Version
+
+SELECT properties.city, count(reservations) as total_reservations
+FROM reservations
+JOIN properties ON property_id = properties.id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
+
+
+--  MY VERSION OF QUERY
+
+-- SELECT city, count(reservations.*) as total_reservations 
+-- FROM properties
+-- JOIN reservations ON  properties.id = property_id
+-- GROUP BY city
+-- ORDER BY total_reservations DESC;
